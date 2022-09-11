50992b2c3687245c6d376d99b7d40c4924ef71ee
vinay krishna <vinay221097@gmail.com>
2022-09-11 20:13:09 +0200

    second

--- api/index.py
+++ api/index.py
@@ -16,3 +16,11 @@ def test():
 def result():
    dict = {'phy':50,'che':60,'maths':70}
    return render_template('result.html', result = dict)
+
+@app.route('/ipt')
+def get_tasks():
+    if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
+        return jsonify({'ip': request.environ['REMOTE_ADDR']}), 200
+    else:
+        return jsonify({'ip': request.environ['HTTP_X_FORWARDED_FOR']}), 200
+
